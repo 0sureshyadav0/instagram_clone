d08455579b1c30b0266018e487a4ab779b83a5ed
Suresh Yadav <suresh.y.np@gmail.com>
2024-08-09 14:39:06 +0545

    adding story

---
+++ lib/data.dart
@@ -0,0 +1,17 @@
+var storyList = {
+  {
+    'storyImage':
+        'https://mediaproxy.salon.com/width/1200/height/675/https://media2.salon.com/2019/04/suprised-man.jpg',
+    'user': 'Your story',
+  },
+  {
+    'storyImage':
+        'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'user': 'Laraa',
+  },
+  {
+    'storyImage':
+        'https://www.shutterstock.com/image-photo/human-face-women-portrait-260nw-296481974.jpg',
+    'user': 'priyakarki777',
+  }
+};
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Instagram '),
     );
   }
 }
@@ -30,39 +32,124 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Colors.transparent,
+        title: Text(widget.title,
+            style: const TextStyle(
+              fontFamily: "DancingScript",
+              fontWeight: FontWeight.w900,
+              letterSpacing: 1.0,
+            )),
+        actions: const [
+          Row(
+            children: [
+              Icon(Icons.heart_broken),
+              SizedBox(width: 20.0),
+              SizedBox(width: 10.0),
+            ],
+          )
+        ],
       ),
-      body: Center(
+      body: bodyPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: Icon(Icons.home_outlined),
+          ),
+          BottomNavigationBarItem(
+            label: "Search",
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            label: "Add",
+            icon: Icon(Icons.add),
+          ),
+          // BottomNavigationBarItem(
+          //   label: "Reels",
+          //   icon: Icon(Icons.play_arrow_rounded),
+          // ),
+          // BottomNavigationBarItem(
+          //   label: "User",
+          //   icon: Icon(Icons.circle),
+          // ),
+        ],
+      ),
+    );
+  }
+}
+
+class bodyPage extends StatefulWidget {
+  const bodyPage({super.key});
+
+  @override
+  State<bodyPage> createState() => _bodyPageState();
+}
+
+class _bodyPageState extends State<bodyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  for (int i = 0; i < storyList.length; i++) ...[
+                    SizedBox(width: 10.0),
+                    Column(
+                      children: [
+                        Stack(
+                          children: [
+                            CircleAvatar(
+                              radius: 30.0,
+                              child: Text(""),
+                            ),
+                            if (i == 0) ...[
+                              // Padding(
+                              //   padding: EdgeInsets.symmetric(
+                              //       horizontal: 35.0, vertical: 40.0),
+                              //   child: Icon(Icons.add),
+                              // ),
+                              // SizedBox(
+                              //   height: 20.0,
+                              //   width: 20.0,
+                              //   child: FloatingActionButton(
+                              //     onPressed: () {},
+                              //     child: Icon(Icons.add),
+                              //   ),
+                              // )
+                            ]
+                          ],
+                        ),
+                        Text("Your Story", style: TextStyle(fontSize: 10.0))
+                      ],
+                    )
+                  ],
+
+                  // CircleAvatar(
+                  //   radius: 30.0,
+                  //   child: Text("Hello"),
+                  // ),
+                  // CircleAvatar(
+                  //   radius: 30.0,
+                  //   child: Text("Hello"),
+                  // ),
+                  // CircleAvatar(
+                  //   radius: 30.0,
+                  //   child: Text("Hello"),
+                  // ),
+                ],
+              ),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
