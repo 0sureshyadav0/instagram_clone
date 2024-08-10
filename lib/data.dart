a3b5c7e23a88710f289683c7552d690f07df8e61
Suresh Yadav <suresh.y.np@gmail.com>
2024-08-10 21:39:38 +0545

    adding story and post

--- lib/data.dart
+++ lib/data.dart
@@ -1,17 +1,26 @@
-var storyList = {
-  {
-    'storyImage':
-        'https://mediaproxy.salon.com/width/1200/height/675/https://media2.salon.com/2019/04/suprised-man.jpg',
-    'user': 'Your story',
-  },
-  {
-    'storyImage':
-        'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    'user': 'Laraa',
-  },
-  {
-    'storyImage':
-        'https://www.shutterstock.com/image-photo/human-face-women-portrait-260nw-296481974.jpg',
-    'user': 'priyakarki777',
-  }
-};
+final storyList = [
+  [
+    "Your story",
+    "https://mediaproxy.salon.com/width/1200/height/675/https://media2.salon.com/2019/04/suprised-man.jpg"
+  ],
+  [
+    "Laraa",
+    "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  ],
+  [
+    "priyakarki777",
+    "https://www.shutterstock.com/image-photo/human-face-women-portrait-260nw-296481974.jpg"
+  ],
+  [
+    "Laraa",
+    "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  ],
+  [
+    "priyakarki777",
+    "https://www.shutterstock.com/image-photo/human-face-women-portrait-260nw-296481974.jpg"
+  ],
+  [
+    "Nature",
+    "https://www.pixelstalk.net/wp-content/uploads/images6/Nature-Wallpaper-High-Resolution.jpg"
+  ]
+];
--- lib/main.dart
+++ lib/main.dart
@@ -46,8 +46,9 @@ class _MyHomePageState extends State<MyHomePage> {
         actions: const [
           Row(
             children: [
-              Icon(Icons.heart_broken),
+              Icon(Icons.favorite_border),
               SizedBox(width: 20.0),
+              // Image(image: AssetImage("./assets/icons/messenger.svg")),
               SizedBox(width: 10.0),
             ],
           )
@@ -92,65 +93,137 @@ class bodyPage extends StatefulWidget {
 class _bodyPageState extends State<bodyPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  for (int i = 0; i < storyList.length; i++) ...[
-                    SizedBox(width: 10.0),
-                    Column(
-                      children: [
-                        Stack(
-                          children: [
-                            CircleAvatar(
-                              radius: 30.0,
-                              child: Text(""),
-                            ),
-                            if (i == 0) ...[
-                              // Padding(
-                              //   padding: EdgeInsets.symmetric(
-                              //       horizontal: 35.0, vertical: 40.0),
-                              //   child: Icon(Icons.add),
-                              // ),
-                              // SizedBox(
-                              //   height: 20.0,
-                              //   width: 20.0,
-                              //   child: FloatingActionButton(
-                              //     onPressed: () {},
-                              //     child: Icon(Icons.add),
-                              //   ),
-                              // )
-                            ]
-                          ],
-                        ),
-                        Text("Your Story", style: TextStyle(fontSize: 10.0))
-                      ],
-                    )
-                  ],
-
-                  // CircleAvatar(
-                  //   radius: 30.0,
-                  //   child: Text("Hello"),
-                  // ),
-                  // CircleAvatar(
-                  //   radius: 30.0,
-                  //   child: Text("Hello"),
-                  // ),
-                  // CircleAvatar(
-                  //   radius: 30.0,
-                  //   child: Text("Hello"),
-                  // ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                for (int i = 0; i < storyList.length; i++) ...[
+                  SizedBox(width: 10.0),
+                  Column(
+                    children: [
+                      Stack(
+                        children: [
+                          CircleAvatar(
+                            radius: 30.0,
+                            backgroundImage: NetworkImage("${storyList[i][1]}"),
+                          ),
+                          if (i == 0) ...[
+                            // Padding(
+                            //   padding: EdgeInsets.symmetric(
+                            //       horizontal: 35.0, vertical: 40.0),
+                            //   child: Icon(Icons.add),
+                            // ),
+                            // SizedBox(
+                            //   height: 20.0,
+                            //   width: 20.0,
+                            //   child: FloatingActionButton(
+                            //     onPressed: () {},
+                            //     child: Icon(Icons.add),
+                            //   ),
+                            // )
+                          ]
+                        ],
+                      ),
+                      Text("${storyList[i][0]}",
+                          style: TextStyle(fontSize: 10.0))
+                    ],
+                  )
                 ],
-              ),
-            )
-          ],
-        ),
+              ],
+            ),
+          ),
+          postList(),
+        ],
       ),
+    );
+  }
+}
+
+class postList extends StatefulWidget {
+  const postList({super.key});
+
+  @override
+  State<postList> createState() => _postListState();
+}
+
+class _postListState extends State<postList> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 10.0),
+        Container(
+          height: MediaQuery.of(context).size.height / 1.3,
+          // color: Colors.red,
+          child: Column(
+            children: [
+              ListTile(
+                // tileColor: Colors.green,
+                contentPadding: EdgeInsets.symmetric(horizontal: 6.0),
+                leading: CircleAvatar(
+                  radius: 15.0,
+                  backgroundImage: NetworkImage(
+                    "${storyList[0][1]}",
+                  ),
+                ),
+                minTileHeight: 5.0,
+                minVerticalPadding: 1.0,
+                title: Text("${storyList[0][0]}",
+                    style: TextStyle(fontSize: 13.0)),
+                subtitle: Text("${storyList[0][0]}",
+                    style: TextStyle(fontSize: 10.0)),
+                trailing: Icon(Icons.menu),
+              ),
+              Container(
+                color: Colors.green,
+                height: MediaQuery.of(context).size.height / 2,
+                child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: Image.network("${storyList[0][1]}")),
+              ),
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Icon(Icons.favorite_border_outlined),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Icon(Icons.chat_rounded),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Icon(Icons.share),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width / 1.7,
+                          ),
+                          Icon(Icons.bookmark_border),
+                        ],
+                      ),
+                      Text("Liked by suvamvlog11 and others",
+                          style: TextStyle(fontSize: 13.0)),
+                      Text("swostika.acharya.00",
+                          style: TextStyle(fontSize: 13.0)),
+                      Text("View 1 comment",
+                          style: TextStyle(fontSize: 13.0, color: Colors.grey)),
+                      Text("53 minutes ago",
+                          style: TextStyle(fontSize: 13.0, color: Colors.grey))
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
